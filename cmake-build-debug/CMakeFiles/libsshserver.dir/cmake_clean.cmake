ce20b25b26567be7c93417503413e6e48d49a427
root <root@localhost.localdomain>
2019-08-24 17:51:42 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/libsshserver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libsshserver.dir/main.c.o"
+  "libsshserver.pdb"
+  "libsshserver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libsshserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
